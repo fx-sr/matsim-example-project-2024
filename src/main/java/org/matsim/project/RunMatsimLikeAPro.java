e23af473cd7fa1babf85fdcbb58d6afadbd13de3
Felix <fxsr@tu-berlin.de>
2024-09-01 22:41:44 +0200

    testingtesting

---
+++ src/main/java/org/matsim/project/RunMatsimLikeAPro.java
@@ -0,0 +1,29 @@
+package org.matsim.project;
+
+import org.matsim.core.config.Config;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RunMatsimLikeAPro {
+
+/*    Config config = null;
+    Collection<String> modes = new ArrayList<>();
+    modes.add( TransportMode.car )
+    modes.add( TransportMode.bike )
+    config.qsim().setMainModes(modes);
+
+    Scenario scenario = ScenarioUtils.loadScenario(config);
+    Controler controler = new Controler(scenario);
+
+    controler.run();*/
+
+
+
+
+
+}
